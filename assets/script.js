86f44dff0cb3efbc07e23636a82a47223d1af865
Dinatolemy <dina.a.tolemy@gmail.com>
2019-12-14 00:19:04 +1100

    optimizing code

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
 
 var APIKey = "45e7195913a3f7dc07bb2a47da166ac8";
-var cloudIcon = $("<img>").attr("src", "https://image.flaticon.com/icons/png/512/1163/1163624.png").css("width", "50", "height", "50");
-var rainIcon = $("<img>").attr("src", "https://image.flaticon.com/icons/png/512/1375/1375394.png").css("width", "50", "height", "50");
-var sunnyIcon = $("<img>").attr("src", "https://image.flaticon.com/icons/png/512/861/861060.png").css("width", "50", "height", "50");
-var PartlySunnyIcon = $("<img>").attr("src","https://image.flaticon.com/icons/png/512/1146/1146869.png").css("width", "50", "height", "50");
+var cloudIcon = "https://image.flaticon.com/icons/png/512/1163/1163624.png";
+var rainIcon = "https://image.flaticon.com/icons/png/512/1375/1375394.png";
+var sunnyIcon = "https://image.flaticon.com/icons/png/512/861/861060.png";
+var PartlySunnyIcon = "https://image.flaticon.com/icons/png/512/1146/1146869.png";
 
 $("#searchButton").on("click", function (event) {
   event.preventDefault();
   displayCityButton();
   displayCityWeather();
 });
+
 function displayCityButton() {
   var city = $("#userCity").val().trim();
   var citylink = $("<li>");
@@ -23,6 +24,7 @@ function displayCityButton() {
   citylink.append(cityButton);
   cityButton.attr("data-name", city);
   $("#cities").append(citylink);
+
   $(cityButton).on("click", function (event) {
     event.preventDefault();
     var cityButtonName = $(this).attr("data-name");
@@ -30,186 +32,67 @@ function displayCityButton() {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function (response) {
-      $("#cityName").text(response.city.name + "  ");
-      if (response.list[0].weather[0].main =="Clear") {
-        $("#cityName").append(sunnyIcon);
-      }
-      else if (response.list[0].weather[0].main =="Rain") {
-        $("#cityName").append(rainIcon);
-      }
-      else if (response.list[0].weather[0].main == "Clouds") {
-        $("#cityName").append(cloudIcon);
-      }
-      else {$("#cityName").append(PartlySunnyIcon)}
-      $("#cityTemp").text("Temp is: " + response.list[0].main.temp);
-      $("#cityHumidity").text("The humidity is : " + response.list[0].main.humidity);
-      $("#cityWindSpeed").text("The wind speed is : " + response.list[0].wind.speed);
-      $("#day1date").text(response.list[8].dt_txt);
+    }).then(callback);
 
-      if (response.list[8].weather[0].main =="Clear") {
-        $("#day1date").append(sunnyIcon);
-      }
-      else if (response.list[8].weather[0].main =="Rain") {
-        $("#day1date").append(rainIcon);
-      }
-      else if (response.list[8].weather[0].main == "Clouds") {
-        $("#day1date").append(cloudIcon);
-      }
-      else { $("#day1date").append(PartlySunnyIcon)}
-      $("#fDayWeather").text("Temp: " + response.list[8].main.temp);
-      $("#fDayHumidity").text("Humidity: " + response.list[8].main.humidity);
-      $("#day2date").text(response.list[16].dt_txt);
-      if (response.list[16].weather[0].main =="Clear") {
-        $("#day2date").append(sunnyIcon);
-      }
-      else if (response.list[16].weather[0].main =="Rain") {
-        $("#day2date").append(rainIcon);
-      }
-      else if (response.list[16].weather[0].main == "Clouds") {
-        $("#day2date").append(cloudIcon);
-      }
-      else { $("#day2date").append(PartlySunnyIcon)}
-      $("#secDayWeather").text("Temp: " + response.list[16].main.temp);
-      $("#secDayHumidity").text("Humidity: " + response.list[16].main.humidity);
-      $("#day3date").text(response.list[24].dt_txt);
-      if (response.list[24].weather[0].main =="Clear") {
-        $("#day3date").append(sunnyIcon);
-      }
-      else if (response.list[24].weather[0].main =="Rain") {
-        $("#day3date").append(rainIcon);
-      }
-      else if (response.list[24].weather[0].main == "Clouds") {
-        $("#day3date").append(cloudIcon);
-      }
-      else { $("#day3date").append(PartlySunnyIcon); }
-      $("#thirdDayWeather").text("Temp: " + response.list[24].main.temp);
-      $("#thirdDayHumidity").text("Humidity: " + response.list[24].main.humidity);
-      $("#day4date").text(response.list[32].dt_txt);
-      if (response.list[32].weather[0].main =="Clear") {
-        $("#day4date").append(sunnyIcon);
-      }
-      else if (response.list[32].weather[0].main =="Rain") {
-        $("#day4date").append(rainIcon);
-      }
-      else if (response.list[32].weather[0].main == "Clouds") {
-        $("#day4date").append(cloudIcon);
-      }
-      else { $("#day4date").append(PartlySunnyIcon); }
-      $("#forthDayWeather").text("Temp: " + response.list[32].main.temp);
-      $("#forthDayHumidity").text("Humidity: " + response.list[32].main.humidity);
-      $("#day5date").text(response.list[39].dt_txt);
-  
-      if (response.list[39].weather[0].main =="Clear") {
-        $("#day5date").append(sunnyIcon);
-      }
-      else if (response.list[39].weather[0].main =="Rain") {
-        $("#day5date").append(rainIcon);
-      }
-      else if (response.list[39].weather[0].main == "Clouds") {
-        $("#day5date").append(cloudIcon);
-      }
-      else { $("#day5date").append(PartlySunnyIcon); }
-      $("#fifthDayWeather").text("Temp: " + response.list[39].main.temp);
-      $("#fifthDayHumidity").text("Humidity: " + response.list[39].main.humidity);
-    })
   });
 }
+
 function displayCityWeather() {
   var city = $("#userCity").val().trim();
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
-    $("#cityName").text(response.city.name + "  ");
-    
-    if (response.list[0].weather[0].main =="Clear") {
-      $("#cityName").append(sunnyIcon);
-    }
-    else if (response.list[0].weather[0].main =="Rain") {
-      $("#cityName").append(rainIcon);
-    }
-    else if (response.list[0].weather[0].main == "Clouds") {
-      $("#cityName").append(cloudIcon);
-    }
-    else { $("#cityName").append(PartlySunnyIcon); }
+  }).then(callback);
+}
+function callback(response) {
+  displayMainSection(response);
+  setDaysOfWeek(response);
+  console.log(response);
+}
+function displayMainSection(response) {
+  $("#cityName").text(response.city.name + "  ");
 
-    $("#cityTemp").text("Temp is: " + response.list[0].main.temp);
-    $("#cityHumidity").text("The humidity is : " + response.list[0].main.humidity);
-    $("#cityWindSpeed").text("The wind speed is : " + response.list[0].wind.speed);
-    $("#day1date").text(response.list[8].dt_txt);
-    if (response.list[8].weather[0].main =="Clear") {
-      $("#day1date").append(sunnyIcon);
-    }
-    else if (response.list[8].weather[0].main =="Rain") {
-      $("#day1date").append(rainIcon);
-    }
-    else if (response.list[8].weather[0].main == "Clouds") {
-      $("#day1date").append(cloudIcon);
-    }
-    else { $("#day1date").append(PartlySunnyIcon); }
-    $("#fDayWeather").text("Temp: " + response.list[8].main.temp);
-    $("#fDayHumidity").text("Humidity: " + response.list[8].main.humidity);
-    $("#day2date").text(response.list[16].dt_txt);
-    if (response.list[16].weather[0].main =="Clear") {
-      $("#day2date").append(sunnyIcon);
-    }
-    else if (response.list[16].weather[0].main =="Rain") {
-      $("#day2date").append(rainIcon);
-    }
-    else if (response.list[16].weather[0].main == "Clouds") {
-      $("#day2date").append(cloudIcon);
-    }
-    else { $("#day2date").append(PartlySunnyIcon); }
-    $("#secDayWeather").text("Temp: " + response.list[16].main.temp);
-    $("#secDayHumidity").text("Humidity: " + response.list[16].main.humidity);
-    $("#day3date").text(response.list[24].dt_txt);
-    if (response.list[24].weather[0].main =="Clear") {
-      $("#day3date").append(sunnyIcon);
-    }
-    else if (response.list[24].weather[0].main =="Rain") {
-      $("#day3date").append(rainIcon);
-    }
-    else if (response.list[24].weather[0].main == "Clouds") {
-      $("#day3date").append(cloudIcon);
-    }
-    else { $("#day3date").append(PartlySunnyIcon); }
-    $("#thirdDayWeather").text("Temp: " + response.list[24].main.temp);
-    $("#thirdDayHumidity").text("Humidity: " + response.list[24].main.humidity);
-    $("#day4date").text(response.list[32].dt_txt);
-    if (response.list[32].weather[0].main =="Clear") {
-      $("#day4date").append(sunnyIcon);
-    }
-    else if (response.list[32].weather[0].main =="Rain") {
-      $("#day4date").append(rainIcon);
-    }
-    else if (response.list[32].weather[0].main == "Clouds") {
-      $("#day4date").append(cloudIcon);
-    }
-    else { $("#day4date").append(PartlySunnyIcon); }
-    $("#forthDayWeather").text("Temp: " + response.list[32].main.temp);
-    $("#forthDayHumidity").text("Humidity: " + response.list[32].main.humidity);
-    $("#day5date").text(response.list[39].dt_txt);
+  if (response.list[0].weather[0].main == "Clear") {
+    $("#cityName").append("<img src='"+sunnyIcon+"' style='width: 50px;'>");
+  }
+  else if (response.list[0].weather[0].main == "Rain") {
+    $("#cityName").append("<img src='"+rainIcon+"' style='width: 50px;'>");
+  }
+  else if (response.list[0].weather[0].main == "Clouds") {
+    $("#cityName").append("<img src='"+cloudIcon+"' style='width: 50px;'>");
+  }
+  else { $("#cityName").append("<img src='"+PartlySunnyIcon+"' style='width: 50px;'>"); }
 
-    if (response.list[39].weather[0].main =="Clear") {
-      $("#day5date").append(sunnyIcon);
-    }
-    else if (response.list[39].weather[0].main =="Rain") {
-      $("#day5date").append(rainIcon);
-    }
-    else if (response.list[39].weather[0].main == "Clouds") {
-      $("#day5date").append(cloudIcon);
-    }
-    else { $("#day5date").append(PartlySunnyIcon); }
-    $("#fifthDayWeather").text("Temp: " + response.list[39].main.temp);
-    $("#fifthDayHumidity").text("Humidity: " + response.list[39].main.humidity);
-   console.log(response);
-  });
+  $("#cityTemp").text("Temp is: " + response.list[0].main.temp);
+  $("#cityHumidity").text("The humidity is : " + response.list[0].main.humidity);
+  $("#cityWindSpeed").text("The wind speed is : " + response.list[0].wind.speed);
 }
 
-
-
-
-
+function setDaysOfWeek(response) {
+  for (var i = 1; i < 6; i++) {
+    var currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + i);
+    currentDate.setHours(9, 0, 0);
+    var formattedDate = moment(currentDate).format("YYYY-MM-DD HH:mm:ss");
+    var item = response.list.filter(x => x.dt_txt === formattedDate)[0];
+    const divId = "#day" + i + "date";
+    const weatherDivId = "#" + i + "DayWeather";
+    const humdityDivId = "#" + i + "DayHumidity";
+    $(divId).text(item.dt_txt);
+    if (item.weather[0].main == "Clear") {
+      $(divId).append("<img src='"+sunnyIcon+"' style='width: 50px;'>");
+    }
+    else if (item.weather[0].main == "Rain") {
+      $(divId).append("<img src='"+rainIcon+"' style='width: 50px;'>");
+    }
+    else if (item.weather[0].main == "Clouds") {
+      $(divId).append("<img src='"+cloudIcon+"' style='width: 50px;'>");
+    }
+    else {
+      $(divId).append("<img src='"+PartlySunnyIcon+"' style='width: 50px;'>");
+    }
+    $(weatherDivId).text("Temp: " + item.main.temp);
+    $(humdityDivId).text("Humidity: " + item.main.humidity);
+  }
+}
